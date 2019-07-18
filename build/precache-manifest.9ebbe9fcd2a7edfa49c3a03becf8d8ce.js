58232d993b7c51f136c0c9ecc2877ab4cfed25da
Chidi Orji <chidi.o@softcom.ng>
2019-07-18 21:28:54 +0100

    chore: build

---
+++ build/precache-manifest.9ebbe9fcd2a7edfa49c3a03becf8d8ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6791d7cae5f6b5d6efa320060a0cc0b1",
+    "url": "/Ethodoxy-Frontend/index.html"
+  },
+  {
+    "revision": "fc4898185d7c14e8ee89",
+    "url": "/Ethodoxy-Frontend/static/css/2.5ec480d0.chunk.css"
+  },
+  {
+    "revision": "16feda9d87d29254c041",
+    "url": "/Ethodoxy-Frontend/static/css/main.225ffaf1.chunk.css"
+  },
+  {
+    "revision": "fc4898185d7c14e8ee89",
+    "url": "/Ethodoxy-Frontend/static/js/2.6bb17d7c.chunk.js"
+  },
+  {
+    "revision": "55ae2244cd7b62b72f0f",
+    "url": "/Ethodoxy-Frontend/static/js/3.f54c7fff.chunk.js"
+  },
+  {
+    "revision": "16feda9d87d29254c041",
+    "url": "/Ethodoxy-Frontend/static/js/main.2df955bc.chunk.js"
+  },
+  {
+    "revision": "a9072f6bd310acd7680f",
+    "url": "/Ethodoxy-Frontend/static/js/runtime~main.58a180c6.js"
+  }
+]);
