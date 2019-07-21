ce7e5bfe80f5457f14154e5d97984c0daa498c12
Chidi Orji <orjichidi95@gmail.com>
2019-07-22 00:14:42 +0100

    Updates

---
+++ precache-manifest.e73797ae6c7a3686587a04dd5e2ff7f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c30887ae77d28107e3f71a00498673",
+    "url": "/Ethodoxy/index.html"
+  },
+  {
+    "revision": "8971140219b61e8a3eb6",
+    "url": "/Ethodoxy/static/css/2.5d832d7a.chunk.css"
+  },
+  {
+    "revision": "01807329f36c078df365",
+    "url": "/Ethodoxy/static/css/main.b676491d.chunk.css"
+  },
+  {
+    "revision": "8971140219b61e8a3eb6",
+    "url": "/Ethodoxy/static/js/2.35bb070b.chunk.js"
+  },
+  {
+    "revision": "7cfeda08eb5249aad762",
+    "url": "/Ethodoxy/static/js/3.c32e13c7.chunk.js"
+  },
+  {
+    "revision": "65e3cd83853ddd98fd78",
+    "url": "/Ethodoxy/static/js/4.3da62355.chunk.js"
+  },
+  {
+    "revision": "0ee51c66ed4cf4c091c3",
+    "url": "/Ethodoxy/static/js/5.1b552df0.chunk.js"
+  },
+  {
+    "revision": "01807329f36c078df365",
+    "url": "/Ethodoxy/static/js/main.22430d85.chunk.js"
+  },
+  {
+    "revision": "11c3768196f05fe6f259",
+    "url": "/Ethodoxy/static/js/runtime~main.e59d4e58.js"
+  }
+]);
